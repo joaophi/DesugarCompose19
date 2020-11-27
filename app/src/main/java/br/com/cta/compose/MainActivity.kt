c024aaab28e1e223dc56b12393ae67046a9a3758
João Pedro Hickmann <joaophickmann@outlook.com>
2020-11-27 10:47:26 -0300

    first commit

---
+++ app/src/main/java/br/com/cta/compose/MainActivity.kt
@@ -0,0 +1,38 @@
+package br.com.cta.compose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import br.com.cta.compose.ui.ComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTheme {
+        Greeting("Android")
+    }
+}
---
+++ desugar/src/main/java/android/util/DesugarView.java
@@ -0,0 +1,18 @@
+package android.util;
+
+import android.view.View;
+
+public class DesugarView {
+    public static void setElevation(View view, float elevation) {
+        // TODO
+    }
+
+    public static boolean getClipToOutline(View view) {
+        // TODO
+        return false;
+    }
+
+    public static void setClipToOutline(View view, boolean clipToOutline) {
+        // TODO
+    }
+}
